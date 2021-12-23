2b4b1e1517deb2d4b274b5ab78c5cc8d020cfae5
jsk56143 <47489757+jsk56143@users.noreply.github.com>
2021-12-23 17:02:36 -0500

    Update tests/integration/models/reddit/test_collections.py
    Co-authored-by: Joel Payne <15524072+LilSpazJoekp@users.noreply.github.com>

--- tests/integration/models/reddit/test_collections.py
+++ tests/integration/models/reddit/test_collections.py
@@ -178,7 +178,7 @@ class TestCollectionModeration(IntegrationTest):
             collection = self.subreddit.collections(uuid)
             with pytest.raises(RedditAPIException) as excinfo:
                 collection.mod.update_display_layout(invalid_layout)
-            assert None == collection.display_layout
+            assert collection.display_layout is None
 
     @mock.patch("time.sleep", return_value=None)
     def test_update_display_layout__lowercase(self, _):
